a0ef0b253a9ae6d1bcc643d11366fed3c6e87bbe
Sadiq <ajdamiaof@gmail.com>
2022-01-05 13:38:50 -0300

    Splash Screen

---
+++ app/src/main/java/com/example/eventsapp/view/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.example.eventsapp.view
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.navigation.Navigation
+import com.example.eventsapp.R
+import com.example.eventsapp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSplashBinding.bind(view)
+
+        val textAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.text_appear_animation)
+        binding.txtAppName.startAnimation(textAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            openHome()
+        }, 3000)
+    }
+
+    private fun openHome() {
+        Navigation.findNavController(requireView()).navigate(R.id.homeFragment)
+    }
+}
